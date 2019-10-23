c73ed575f2ab497e8dcbee52b1aa32283833ffa9
Victor Cardona <victorhugocardonagomez@gmail.com>
2019-10-23 11:59:40 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# time_tracker_app
+ Time Tracker App Using Flutter
---
+++ lib/app/sign_in/sign_in_page.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:time_tracker_app/app/sign_in/sign_in_button.dart';
+import 'package:time_tracker_app/common_widgets/custom_raised_button.dart';
+
+class SignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amberAccent[200],
+        title: Text('Time Tracker'),
+        elevation: 8.0,
+        centerTitle: true,
+      ),
+      body: _buildContent(),
+      backgroundColor: Colors.grey[200],
+    );
+  }
+
+  Widget _buildContent() {
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Text(
+            'Sign In',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 32.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(
+            height: 48.0,
+          ),
+          CustomRaisedButton(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(
+                    'images/google-logo.png',
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text('Sign in with Google'),
+                ]),
+            color: Colors.white,
+            onPressed: () {},
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          CustomRaisedButton(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(
+                    'images/facebook-logo.png',
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    'Sign in with Facebook',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ]),
+            color: Color(0xFF334D92),
+            onPressed: () {},
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          SignInButton(
+            text: 'Sign in with email',
+            textColor: Colors.white,
+            color: Colors.teal[700],
+            onPressed: () {},
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          Text(
+            'or',
+            style: TextStyle(fontSize: 14.0, color: Colors.black87),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          SignInButton(
+            text: 'Go anonymoous',
+            textColor: Colors.black,
+            color: Colors.lime[300],
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
